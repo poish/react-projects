87f025076baa7cdb8fcbe6d0e255fb904cfdb361
Robert Rudnik <r.rudnik.cpp@gmail.com>
2022-02-11 19:44:16 +0100

    first commit

---
+++ to_do_list/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ToDo from './components/todo';
+
+function App() {
+  return (
+    <div className="App">
+      <ToDo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ to_do_list/src/components/todo.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import '../styles/main.css'
+
+class ToDo extends React.Component {
+
+    render(){
+
+        return <ul>
+            <li>first</li>
+            <li>second</li>
+            <li>third</li>
+        </ul>;
+    }
+}
+
+export default ToDo;
